f3ef4cd2f59c61cec8fd7413046c257ba00b4313
ptiwari6239 <93005504+ptiwari6239@users.noreply.github.com>
2025-02-15 22:00:40 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
 echo "hello  from gi  thub"
-pwd
+scp index.nginx-debian.html ubuntu@10.0.10.66:/home/ubuntu
+ssh ubuntu@10.0.10.66
+sudo su 
+rm /var/www/html/index.nginx-debian.html
+mv /home/ubuntu/index.nginx-debian.html /var/www/html/
